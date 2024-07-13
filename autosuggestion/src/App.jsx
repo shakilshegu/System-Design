89030bafd04a7af3c80020f9cae24456db350fe0
shakilshagu <muhammadshakil1968@gmail.com>
2024-07-13 18:30:07 +0530

    implemented  Advanced  autosuggestions Accessibility and Caching and Security

--- autosuggestion/src/App.jsx
+++ autosuggestion/src/App.jsx
@@ -1,20 +1,46 @@
-
-import Autocomplete from './components/autocomplete'
+import Autocomplete from "./components/autocomplete";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  // const staticData = [
+  //   "Apple",
+  //   "Mangos",
+  //   "Banana",
+  //   "Orange",
+  //   "Melon",
+  //   "Cherry",
+  //   "Peach",
+  //   "Plum",
+  // ]
+  const fetchSuggestions = async (query) => {
+  const response = await fetch(
+    `https://dummyjson.com/recipes/search?q=${query}`
+  );
+  if(!response.ok) {
+    throw new Error("NetWork response was not ok ")
+  }
+  const result = await response.json();
+  return result.recipes;
 
-const fetchSuggestions = async () =>{
-
-}
-
+  };
+ 
   return (
     <>
-    <h1>Hello Its Shakil!!!</h1>
-    <Autocomplete placeholder={"Enter Recipe"} 
-      fetchSuggestions={fetchSuggestions}
-    />
+      <h1>Autocomplete / Typeahead </h1>
+      <Autocomplete
+        placeholder={"Enter Recipe"}
+        // staticData={staticData}
+        fetchSuggestions={fetchSuggestions}
+        dataKey={"name"}
+        customLoading={<>Loading Recipes.. </>}
+        onSelect={(res) => console.log(res)}
+        onChange={(input)=> {}}
+        onBlur={(e)=> {}}
+        onFocus={(e)=> {}}
+        customStyles={{}}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- autosuggestion/src/components/autocomplete.jsx
+++ autosuggestion/src/components/autocomplete.jsx
@@ -1,9 +1,92 @@
+/* eslint-disable react/prop-types */
+import { useCallback, useEffect, useState } from "react";
+import "./styles.css";
+import SuggestionList from "./suggestions-list";
+import debounce from "lodash/debounce"
+const Autocomplete = ({
+  staticData,
+  fetchSuggestions,
+  placeholder = "",
+  customLoading = "Loading..",
+  onSelect = () => {},
+  onBlur = () => {},
+  onFocus = () => {},
+  onChange = () => {},
+  customStyles = {},
+  dataKey = "",
+}) => {
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  console.log(suggestions);
 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    onChange(event.target.value);
+    suggestions([])
+  };
 
-const Autocomplete = () => {
+  const getSuggestions = async (query) => {
+    setError(null);
+    setLoading(true);
+    try {
+      let result;
+      if (staticData) {
+        result = staticData.filter((item) => {
+          return item.toLowerCase().includes(query.toLowerCase());
+        });
+      } else if (fetchSuggestions) {
+        result = await fetchSuggestions(query);
+      }
+      setSuggestions(result);
+    } catch (error) {
+      setError("Failed to fetch suggestions");
+      setSuggestions([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const getSuggestionsDebounced = useCallback(debounce(getSuggestions,300),[]);
+  useEffect(() => {
+    if (inputValue.length > 1) {
+      getSuggestionsDebounced(inputValue);
+    } else {
+      setSuggestions([]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValue]);
+
+  const handleSuggestionClick = (suggestion) =>{
+  setInputValue(dataKey ? suggestion[dataKey]: dataKey)
+  onSelect(suggestion)
+  }
+
   return (
-    <div>autocomplete</div>
-  )
-}
+    <div className="container">
+      <input
+        type="text"
+        value={inputValue}
+        placeholder={placeholder}
+        style={customStyles}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        onChange={handleInputChange}
+      />
+      {(suggestions.length > 0 || loading || error) && (
+        <ul className="suggestions-list ">
+          {error && <div className="error">{error}</div>}
+          {loading && <div className="loading">{customLoading}</div>}
+          <SuggestionList 
+          dataKey={dataKey}
+          highlight={inputValue}
+          suggestions={suggestions}
+          onSuggestionClick={handleSuggestionClick}
+          />
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default Autocomplete
+export default Autocomplete;
